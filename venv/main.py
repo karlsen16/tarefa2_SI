770fe6960723e4f9ebf9e80bbfc4757c307f9ef4
karlsen16 <l.rdt@pm.me>
2023-10-15 17:43:20 -0300

    alg id3

--- venv/main.py
+++ venv/main.py
@@ -1,6 +1,26 @@
 import csv
+import matplotlib.pyplot as plt
+from sklearn import tree
+# from sklearn.datasets import load_iris
+import numpy as np
 
-with open('com_label.txt', newline='') as csvfile:
+with open('test.txt', newline='') as csvfile:
     data = list(csv.reader(csvfile))
 
-print(data)
+# print(data)
+
+# data = [[0, 0], [1, 1]]
+Y = np.arange(1, 11, 1)
+print(Y)
+clf = tree.DecisionTreeClassifier(criterion="entropy")
+clf = clf.fit(data, Y)
+clf.predict([[2., 2., 2., 2., 2., 2., 2., 2.]])
+# clf.predict_proba([[2., 2., 2., 2., 2., 2., 2., 2.]])
+
+# iris = load_iris()
+# data, y = iris.data, iris.target
+# clf = tree.DecisionTreeClassifier()
+# clf = clf.fit(data, y)
+
+tree.plot_tree(clf)
+plt.show()
