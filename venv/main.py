640b6e913986443c17f65981362f88d2565c6963
karlsen16 <l.rdt@pm.me>
2023-10-21 20:25:00 -0300

    alg CART

--- venv/main.py
+++ venv/main.py
@@ -1,26 +1,36 @@
-import csv
 import matplotlib.pyplot as plt
+# import numpy as np
+import pandas as pd
 from sklearn import tree
-# from sklearn.datasets import load_iris
-import numpy as np
+from sklearn.model_selection import train_test_split
+# from sklearn.externals import joblib
 
-with open('test.txt', newline='') as csvfile:
-    data = list(csv.reader(csvfile))
+df = pd.read_csv('com_label.txt', names=['ID', 'pSist', 'pDiast',
+                                         'qPA', 'pulso', 'respiracao', 'gravidade',
+                                         'saida']).iloc[:, 1:]
+# print(df.head())
 
-# print(data)
+# X = df[['qPA', 'pulso', 'respiracao']].to_numpy()
+X = df[['qPA', 'pulso', 'respiracao', 'gravidade']].to_numpy()
+# X = df[['gravidade']].to_numpy()
+Y = df['saida'].to_numpy()
+# Y = df['gravidade'].to_numpy()
 
-# data = [[0, 0], [1, 1]]
-Y = np.arange(1, 11, 1)
-print(Y)
-clf = tree.DecisionTreeClassifier(criterion="entropy")
-clf = clf.fit(data, Y)
-clf.predict([[2., 2., 2., 2., 2., 2., 2., 2.]])
-# clf.predict_proba([[2., 2., 2., 2., 2., 2., 2., 2.]])
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
-# iris = load_iris()
-# data, y = iris.data, iris.target
-# clf = tree.DecisionTreeClassifier()
-# clf = clf.fit(data, y)
-
-tree.plot_tree(clf)
+clf = tree.DecisionTreeClassifier(criterion='entropy')
+# clf = tree.DecisionTreeRegressor(max_depth=3)
+clf = clf.fit(X_train, Y_train)
+tree.plot_tree(clf, feature_names=['qPA', 'pulso', 'respiracao', 'gravidade'],
+               class_names=['critico', 'instavel', 'potencial estavel', 'estavel'])
+# tree.plot_tree(clf, feature_names=['qPA', 'pulso', 'respiracao'],
+#                class_names=['critico', 'instavel', 'potencial estavel', 'estavel'])
 plt.show()
+
+clf = clf.predict(X_test)
+
+df_saida = pd.DataFrame({'ID3': clf, 'Y_test': Y_test})
+print(df_saida)
+# # saving
+# # filename = 'model.sav'
+# # joblib.dump(clf, filename)
